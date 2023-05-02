d4c95ae985860804e8c02d81bcb58291623a2751
watakazu-git <132337028+watakazu-git@users.noreply.github.com>
2023-05-02 17:01:09 +0900

    feat: .gitignoreを作成する

--- SelfIntroduction/SelfIntroduction/SelfIntroduction/ViewController.swift
+++ SelfIntroduction/SelfIntroduction/SelfIntroduction/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var pageControl: UIPageControl!
     
-    
+    //aaaa
     
     
     let imageArr = [

2cd5f94080a449f5819edcf092f96b4687d5b6d7
watakazu-git <132337028+watakazu-git@users.noreply.github.com>
2023-05-04 18:07:10 +0900

    Create CalenderViewController.swift

---
+++ SelfIntroductionApp/SelfIntroductionApp/SelfIntroductionApp/CalenderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalenderViewController.swift
+//  SelfIntroductionApp
+//
+//  Created by Watanabe Kazuyuki on 2023/05/04.
+//
+
+import UIKit
+
+class CalenderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        let selection = UICalendarSelectionSingleDate(delegate: self)
+        let calendarView = UICalendarView()
+        calendarView.frame = view.bounds
+        calendarView.delegate = self
+        calendarView.selectionBehavior = selection
+        view.addSubview(calendarView)
+    }
+}
+extension CalenderViewController: UICalendarViewDelegate {}
+extension CalenderViewController: UICalendarSelectionSingleDateDelegate {
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        guard let dateComponents = dateComponents,
+              let year = dateComponents.year,
+              let month = dateComponents.month,
+              let day = dateComponents.day else {
+            return
+        }
+        print("yate: \(year), month: \(month), day: \(day)")
+    }
+}

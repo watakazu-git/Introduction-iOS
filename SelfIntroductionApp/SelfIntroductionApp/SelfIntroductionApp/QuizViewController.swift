dc8b88a14f8d695acea43534a75d25cba7ff823e
watakazu-git <132337028+watakazu-git@users.noreply.github.com>
2023-05-07 23:44:01 +0900

    Create QuizViewController.swift

---
+++ SelfIntroductionApp/SelfIntroductionApp/SelfIntroductionApp/QuizViewController.swift
@@ -0,0 +1,118 @@
+//
+//  QuizViewController.swift
+//  SelfIntroductionApp
+//
+//  Created by Watanabe Kazuyuki on 2023/05/07.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    @IBOutlet var quizNumberLabel: UILabel!
+    @IBOutlet var quizTextView: UITextView!
+    @IBOutlet var answerButton1: UIButton!
+    @IBOutlet var answerButton2: UIButton!
+    @IBOutlet var answerButton3: UIButton!
+    @IBOutlet var answerButton4: UIButton!
+    @IBOutlet var judgeImageView: UIImageView!
+    
+    
+    var csvArray: [String] = []
+    var quizArray: [String] = []
+    var quizCount = 0
+    var correctCount = 0
+    var selectLebel = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("選択したのはレベル\(selectLebel)")
+        
+        csvArray = loadCSV(fileName: "quiz\(selectLebel)")
+        csvArray.shuffle()
+        
+        quizArray = csvArray[quizCount].components(separatedBy: ",")
+        quizNumberLabel.text = "第\(quizCount + 1)問"
+        quizTextView.text = quizArray[0]
+        answerButton1.setTitle(quizArray[2], for: .normal)
+        answerButton2.setTitle(quizArray[3], for: .normal)
+        answerButton3.setTitle(quizArray[4], for: .normal)
+        answerButton4.setTitle(quizArray[5], for: .normal)
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //画面遷移するときにcorrectに正解数を渡す
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let scoreVC = segue.destination as! ScoreViewController
+        scoreVC.correct = correctCount
+    }
+    
+    //ボタンが押されるとtagをprint
+    @IBAction func btnAction(sender: UIButton) {
+        if sender.tag == Int(quizArray[1]) {
+            print("正解")
+            correctCount += 1
+            judgeImageView.image = UIImage(named: "correct")
+        } else {
+            print("不正解")
+            judgeImageView.image = UIImage(named: "incorrect")
+        }
+        judgeImageView.isHidden = false
+        answerButton1.isEnabled = false
+        answerButton2.isEnabled = false
+        answerButton3.isEnabled = false
+        answerButton4.isEnabled = false
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.judgeImageView.isHidden = true
+            self.answerButton1.isEnabled = true
+            self.answerButton2.isEnabled = true
+            self.answerButton3.isEnabled = true
+            self.answerButton4.isEnabled = true
+            self.nextQuiz()  //クロージャ
+        }
+    }
+    
+    func nextQuiz() {
+        quizCount += 1
+        
+        //画面遷移の判定
+        if quizCount < csvArray.count {
+            quizArray = csvArray[quizCount].components(separatedBy: ",")
+            quizNumberLabel.text = "第\(quizCount + 1)問"
+            quizTextView.text = quizArray[0]
+            answerButton1.setTitle(quizArray[2], for: .normal)
+            answerButton2.setTitle(quizArray[3], for: .normal)
+            answerButton3.setTitle(quizArray[4], for: .normal)
+            answerButton4.setTitle(quizArray[5], for: .normal)
+        } else {
+            performSegue(withIdentifier: "toScoreVC", sender: nil)
+        }
+    }
+    
+    //csvデータを配列に変換
+    func loadCSV(fileName: String) -> [String] {
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            let csvData = try String(contentsOfFile: csvBundle, encoding: String.Encoding.utf8)
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            csvArray = lineChange.components(separatedBy: "\n")
+            csvArray.removeLast()
+        } catch {
+            print("エラー")
+        }
+        return csvArray
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
